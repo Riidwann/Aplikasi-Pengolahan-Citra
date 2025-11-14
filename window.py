f888066ff15cf750beb6882334aa4605e802e4a5
waznet <muridwan2005@gmail.com>
2025-11-14 07:23:45 +0700

    Create branch Ridwan

--- window.py
+++ window.py
@@ -3,7 +3,6 @@ from tkinter import *
 #Setup window
 window = Tk()
 window.geometry("600x500")
-window.title("Aplikasi Pengolahan Citra")
-
+window.title("Aplikasi Pengolahan Citra Ganteng Banget")
 
 window.mainloop()
